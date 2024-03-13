6bbb1d5cd5725dec6acd82c2edcd4cc6f9b4b3bc
Zoya Torosyan <javahk98@bk.ru>
2024-03-13 17:18:59 +0300

    Init

---
+++ Helpers.cpp
@@ -0,0 +1,6 @@
+#include "Helpers.h"
+
+int sum(int a, int b)
+{
+	return (a + b)*(a + b);
+}
---
+++ ProjectH.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int result = sum(12, 25);
+    std::cout << "Result:" << result << "\n";
+    return 0;
+}
